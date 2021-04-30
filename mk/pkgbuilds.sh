644c7971f389f04d2d695de482bad5a056088e55
Andy Kluger <andykluger@gmail.com>
2021-04-30 01:35:07 -0400

    improve path generation reliability in mk/pkgbuilds.sh

--- mk/pkgbuilds.sh
+++ mk/pkgbuilds.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -e
 
-gitroot="$(git rev-parse --show-toplevel)"
+gitroot="$(git -C "$(dirname "$0")" rev-parse --show-toplevel)"
 
 "${gitroot}"/mk/private-build-plans.toml.sh
 
-pbp_sha256="$(sha256sum "${gitroot}"/private-build-plans.toml | cut -d' ' -f 1)"
+pbp_sha256="$(sha256sum "${gitroot}"/private-build-plans.toml | cut -d ' ' -f 1)"
 
 if [ "$(yaml-get -p build_webfonts "${gitroot}"/vars.yml)" = yes ]; then
   build_webfonts=true
