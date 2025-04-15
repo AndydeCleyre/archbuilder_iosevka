c4cd25e70d5c5e4830c69310fa933ebab5cadb14
Andy Kluger <AndyKluger@gmail.com>
2025-04-15 12:31:26 -0400

    Add option to build hinted or unhinted, set unhinted for now

--- mk/pkgbuilds.sh
+++ mk/pkgbuilds.sh
@@ -17,6 +17,7 @@ pbp_sha256="$(sha256sum "${gitroot}"/private-build-plans.toml | cut -d ' ' -f 1)
 vars="$(mktemp)"
 nt2json -s "${gitroot}"/vars.types.nt "${gitroot}"/vars.nt >"$vars"
 
+hinted="$(yaml-get -p hinted "$vars")"
 build_webfonts="$(yaml-get -p build_webfonts "$vars")"
 upstream_branch="$(yaml-get -p branch "$vars")"
 use_custom_name="$(yaml-get -p use_custom_name "$vars")"
@@ -37,6 +38,7 @@ yaml-get -p 'spacings.*' "$vars" | while read -r spacing; do
     printf '%s\n' '{}' | \
     yaml-set -g spacing -a "$spacing" | \
     yaml-set -g pbp_sha256 -a "$pbp_sha256" | \
+    yaml-set -g hinted -a "$hinted" | \
     yaml-set -g build_webfonts -a "$build_webfonts" | \
     yaml-set -g branch -a "$upstream_branch" | \
     yaml-set -g use_custom_name -a "$use_custom_name" | \
